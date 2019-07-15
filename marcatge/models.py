e2c3c31cba2e79487e0735a2f0d02aca3180dd61
Eduard Berloso Clarà <eduard.bc.95@gmail.com>
2019-07-15 18:06:19 +0200

    Better msg

--- marcatge/models.py
+++ marcatge/models.py
@@ -55,7 +55,7 @@ class Treballador(models.Model):
         total_spent = timedelta(seconds=0)
         for marcat_avui in marcats_avui:
             total_spent += marcat_avui.get_time_spent()
-        msg += _("\nTotal marcat avui: {0}.").format(str(total_spent).split(".")[0])
+        msg = _("Total marcat avui: <b>{0}</b>.\n").format(str(total_spent).split(".")[0]) + msg
         return msg
 
 
